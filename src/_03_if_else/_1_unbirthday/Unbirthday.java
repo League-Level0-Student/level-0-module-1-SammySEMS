ab80afa3c1ea154386643454f27073b7fcf2ac67
root <root@d0ad6b3927e9>
2020-11-09 21:28:50 +0000

    Secret Message BoxIP

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,20 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	
+	String birthday = JOptionPane.showInputDialog ("When is your birthday (mm/dd)");
+	
+if(birthday.equalsIgnoreCase("11/09")) {
+	JOptionPane.showMessageDialog(null, "Happy Birthday to you!");
+	
+	
+}
+else {
+	JOptionPane.showMessageDialog(null, "Happy UnBirthday to you!");
+}
+}
+
+}
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -10,9 +10,16 @@ public static void main(String[] args) {
 		// 1. Save something remarkable about each person in a variable.
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+	String HelloKitty = "Cute bow";
+String superman = "Great flying";
+String other = "Are you a Superhero?";
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
-	}
+String name = JOptionPane.showInputDialog("Who are you?");
+if (name.equalsIgnoreCase("Hello Kitty")) {
+	
+JOptionPane.showMessageDialog(null, HelloKitty);}
+else if (name.equalIgnoreCase("Superman")) {
 }
 
+}
+}
